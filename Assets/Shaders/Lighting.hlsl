e0f72b9182fdb57dddbf09ee3a28ffa6af6518e2
Joao Victor Oliveira <joaocrft@gmail.com>
2024-09-21 01:16:21 -0300

    Pre2022

--- Assets/Shaders/Lighting.hlsl
+++ Assets/Shaders/Lighting.hlsl
@@ -18,6 +18,27 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
+void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out half DistanceAtten, out half ShadowAtten) {
+#ifdef SHADERGRAPH_PREVIEW
+    Direction = half3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+#if SHADOWS_SCREEN
+    half4 clipPos = TransformWorldToHClip(WorldPos);
+    half4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
+#endif
+}
+
 void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView,
     float MainDiffuse, float MainSpecular, float3 MainColor,
     out float Diffuse, out float Specular, out float3 Color) {
